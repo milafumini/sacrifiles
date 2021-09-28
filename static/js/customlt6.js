70bf1d2c3ec239519721b5764cb87335387c14ce
aschimmenti <andschimmenti@gmail.com>
2021-09-28 16:34:37 +0200

    general fixes

--- static/js/customlt6.js
+++ static/js/customlt6.js
@@ -10,7 +10,7 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-$.get("../data/literature/lt_2_religion.csv", function (CSVdata) {
+$.get("../data/literature/lt_6_self2.csv", function (CSVdata) {
     var options = { "separator": ";" };
     data = $.csv.toObjects(CSVdata, options);
     const App = new Vue({
